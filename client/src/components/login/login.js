7ff052427401bc56b1dd266f5faf6f7db15a2b84
reshm <reshmapsuresh20@gmail.com>
2024-12-07 21:35:37 +0530

    feat : Display more fields when user clicks on Register button

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,26 +1,70 @@
 import React, { useState } from "react";
 const Login = () => {
 
-const [formData,setFormData] = useState({
+const [isRegister, setIsRegister] = useState(false);
+const [formData, setFormData] = useState({
     username: "",
+    firstName: "",
+    lastName: "",
     password: "",
+    confirmPassword: "",
   });
 
 const handleChange = (e) => {
  setFormData({ ...formData, [e.target.name]: e.target.value });
+};
+
+const handleLogin = (e) => {
   };
+
+const handleNewUser = (e) => {
+    setIsRegister(!isRegister);
+    setFormData({ username: "", password: "", firstName: "", lastName: "", confirmPassword: "" });
+  };
+
+const handleRegister = (e) => {
+  };
+
 
  return (
     <div className="login-container">
       <div className="login-form">
-        <h1>Login</h1>
+       <h1 data-testid="login-heading">{isRegister ? "Register" : "Login"}</h1>
 
          <div className="form-group">
             <input type="text" name="username" placeholder="Username" value={formData.username}
                     onChange={handleChange} />
+         </div>
+         {isRegister && ( <>
+            <div className="form-group">
+                <input type="text" name="firstName" placeholder="First Name" value={formData.firstName}
+                            onChange={handleChange} />
+            </div>
+            <div className="form-group">
+                <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName}
+                           onChange={handleChange} />
+            </div>
+            </>
+         )}
+         <div className="form-group">
             <input type="password" name="password" placeholder="Password" value={formData.password}
                     onChange={handleChange} />
          </div>
+         {isRegister && ( <>
+            <div className="form-group confirm-password-container">
+                <input type="password" name="confirmPassword" placeholder="Confirm Password" value={formData.confirmPassword}
+                          onChange={handleChange}/>
+            </div>
+            </>
+         )}
+        <button className="form-button" data-testid="login-id"
+          onClick={isRegister ? handleRegister : handleLogin} >
+            {isRegister ? "Register" : "Login"}
+        </button>
+        <button className={`form-button ${isRegister ? 'already-user' : ''}`} data-testid="register-id"
+          onClick={handleNewUser} >
+          {isRegister ? "Already a user?" : "New User"}
+        </button>
       </div>
     </div>
  );
--- client/src/components/login/login.test.js
+++ client/src/components/login/login.test.js
@@ -7,7 +7,7 @@ import Login from "./login";
 test('Displays Login header in the Login page', () => {
 
     render(<Login />);
-    const titleElement = screen.getByText(/Login/i);
+    const titleElement = screen.getByTestId('login-heading');
     expect(titleElement).toBeInTheDocument();
 })
 
@@ -22,3 +22,29 @@ test('Displays Username and password fields', () => {
     fireEvent.change(inputPassword, { target: { value: "User123" } });
     expect(inputPassword.value).toBe("User123");
 })
+
+ test("Display Login and New User button on initial load", () => {
+
+    render(<Login />);
+    const loginButton = screen.getByTestId('login-id');
+    expect(loginButton).toBeInTheDocument();
+
+    const newUserButton = screen.getByTestId('register-id');
+    expect(newUserButton).toBeInTheDocument();
+  });
+
+  test("Display more fields when user clicks on New User button", () => {
+
+      render(<Login />);
+        const registerButton = screen.getByTestId('register-id');
+        fireEvent.click(registerButton);
+        expect(screen.getByPlaceholderText('First Name')).toBeInTheDocument();
+        expect(screen.getByPlaceholderText('Last Name')).toBeInTheDocument();
+        expect(screen.getByPlaceholderText('Confirm Password')).toBeInTheDocument();
+
+        const loginButton = screen.getByTestId('login-id');
+        expect(loginButton).toBeInTheDocument();
+
+        const newUserButton = screen.getByTestId('register-id');
+        expect(newUserButton).toBeInTheDocument();
+  });
