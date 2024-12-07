8c9f86cb21773b6e014a51439a9be335a95a05e1
reshm <reshmapsuresh20@gmail.com>
2024-12-07 18:05:57 +0530

    feat : Login header should be displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/login/login";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
