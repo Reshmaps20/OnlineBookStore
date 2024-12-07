6fef66f4a8351827c339b71b17ed1cc4fd6d8690
reshm <reshmapsuresh20@gmail.com>
2024-12-07 18:26:56 +0530

    feat : Display Username and password fields

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,9 +1,26 @@
+import React, { useState } from "react";
 const Login = () => {
+
+const [formData,setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+const handleChange = (e) => {
+ setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
  return (
     <div className="login-container">
       <div className="login-form">
         <h1>Login</h1>
+
+         <div className="form-group">
+            <input type="text" name="username" placeholder="Username" value={formData.username}
+                    onChange={handleChange} />
+            <input type="password" name="password" placeholder="Password" value={formData.password}
+                    onChange={handleChange} />
+         </div>
       </div>
     </div>
  );
--- client/src/components/login/login.test.js
+++ client/src/components/login/login.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Login from "./login";
 
@@ -9,4 +9,16 @@ test('Displays Login header in the Login page', () => {
     render(<Login />);
     const titleElement = screen.getByText(/Login/i);
     expect(titleElement).toBeInTheDocument();
+})
+
+test('Displays Username and password fields', () => {
+
+    render(<Login />);
+    const inputUsername = screen.getByPlaceholderText("Username");
+    fireEvent.change(inputUsername, { target: { value: "User" } });
+    expect(inputUsername.value).toBe("User");
+
+    const inputPassword = screen.getByPlaceholderText("Password");
+    fireEvent.change(inputPassword, { target: { value: "User123" } });
+    expect(inputPassword.value).toBe("User123");
 })
