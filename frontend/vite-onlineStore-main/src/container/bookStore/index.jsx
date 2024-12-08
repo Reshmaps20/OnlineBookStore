b8afcc18ed5f564bf3955f5fb0fae638c464cff7
reshm <reshmapsuresh20@gmail.com>
2024-12-08 19:14:20 +0530

    feat : book store api fetch

--- frontend/vite-onlineStore-main/src/container/bookStore/index.jsx
+++ frontend/vite-onlineStore-main/src/container/bookStore/index.jsx
@@ -6,6 +6,33 @@ import "./index.css";
 const BookStore = () => {
   const location = useLocation();
   const { username, password } = location.state || {};
+  const [books, setBooks] = useState([]);
+  const [error, setError] = useState(null);
+
+  const credentials = `${username}:${password}`;
+  const encodedCredentials = btoa(credentials);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+const fetchBooks = () => {
+    setError(null);
+    console.log("credentials:",credentials);
+    axios
+      .get("http://localhost:8080/api/books", {
+        headers: {
+          Authorization: `Basic ${encodedCredentials}`,
+        },
+      })
+      .then((response) => {
+        setBooks(response.data);
+        console.log("Books retrieved:", response.data);
+      })
+      .catch((error) => {
+        setError("Error fetching books. Please try again.");
+      });
+  };
 
   return (
     <div className="bookscreen-container">
@@ -17,6 +44,17 @@ const BookStore = () => {
         <span className="username" data-testid="user-greeting">Hi,{username}</span>
               <button className="cart-btn" data-testid="cart-button"> Cart  </button>
       </div>
+      <div className="book-list">
+              {books.map((book) => (
+                <div key={book.id} className="book-item" data-testid="book-item">
+                  <h3>{book.title}</h3>
+                  <p>Author: {book.author}</p>
+                  <p>Price: €{book.price}</p>
+
+
+                </div>
+              ))}
+            </div>
     </div>
   );
 };
--- frontend/vite-onlineStore-main/src/container/bookStore/index.test.jsx
+++ frontend/vite-onlineStore-main/src/container/bookStore/index.test.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import axios from "axios";
 import { BrowserRouter as Router } from "react-router-dom";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import BookStore from "./index";
 
+afterEach(() => {
+  jest.clearAllMocks();
+  axios.get.mockClear();
+})
+
+
+jest.mock("axios");
 
 test('Displays Online BookStore Heading', () => {
+
+
+ const mockBooksData = [
+       { id: 1, title: 'Book 1' },
+       { id: 2, title: 'Book 2' },
+     ];
+  axios.get.mockResolvedValueOnce({ data: mockBooksData });
 
         render(
           <Router initialEntries={["/books", { state: { username: "username", password: "abc" } }]}>
@@ -20,4 +35,29 @@ test('Displays Online BookStore Heading', () => {
     expect(cartButton).toBeInTheDocument();
     const logoutButton = screen.getByTestId('logout-button');
     expect(logoutButton).toBeInTheDocument();
+})
+
+test('Fetch All the books and displaye in the screen', () => {
+
+ const mockBooksData = [
+       { id: 1, title: 'Book 1' },
+       { id: 2, title: 'Book 2' },
+     ];
+  axios.get.mockResolvedValueOnce({ data: mockBooksData });
+
+        render(
+          <Router initialEntries={["/books", { state: { username: "username", password: "abc" } }]}>
+            <BookStore />
+          </Router>
+        );
+//     const response = await waitFor(() =>
+//                 screen.getByTestId('book-item')
+//          );
+    const bookStoreTitle = screen.getByTestId('bookstore-heading');
+    expect(bookStoreTitle).toBeInTheDocument();
+
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:8080/api/books', {
+          headers: { Authorization: `Basic dW5kZWZpbmVkOnVuZGVmaW5lZA==` },
+        });
+
 })
